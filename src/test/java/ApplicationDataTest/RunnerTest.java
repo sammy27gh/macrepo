a00e6fad7231ab0b60a09eb97015f357068e289f
samuel andoh <sammy27gh@yahoo.com>
2018-08-15 15:37:21 -0400

    added a new reporting system

--- src/test/java/ApplicationDataTest/RunnerTest.java
+++ src/test/java/ApplicationDataTest/RunnerTest.java
@@ -24,7 +24,31 @@ public class RunnerTest {
 		
 }*/
 
+import org.junit.runner.RunWith;
 
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
+        retryCount = 3,
+        detailedReport = true,
+        detailedAggregatedReport = true,
+        overviewReport = false,
+        toPDF = false,
+        outputFolder = "target")
+@CucumberOptions(plugin = { "html:target/cucumber-html-report",
+        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "usage:target/cucumber-usage.json",
+        "junit:target/cucumber-results.xml"
+        },
+        features = { "src/test/resources/features" },
+        glue = { "ApplicationDataTest" },
+        tags = {})
+public class RunnerTest {
+}
 
 
 
